951f77c4188a381054f864455fa818e6ea2b93cf
locnhuvu <locnhuvu@gmail.com>
2019-06-19 23:09:32 +0700

    time layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'store';
+import App from './store';
 import 'utils/i18n';
 ReactDOM.render(<App />, document.getElementById('root'));
