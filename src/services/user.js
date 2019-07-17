8cd974da1fc680a2bc2ce24d6c618ad6cf8afde8
Khoa Mai <maidinhkhoa92@gmail.com>
2019-07-18 00:39:18 +0700

    Fixed: esslint

--- src/services/user.js
+++ src/services/user.js
@@ -4,7 +4,7 @@ const Register = (params, token) => {
     url: '/wp/v2/users',
     method: 'post',
     headers: {
-      'Authorization': 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRvcjMxOTkudGVtcC5kb21haW5zXC9-bWlyYWNsZXNcL3ZpbmhsaXZlcnBvb2xcL2Jhb2hpZW1kYXRhIiwiaWF0IjoxNTYzMzgxMjk5LCJuYmYiOjE1NjMzODEyOTksImV4cCI6MTU2Mzk4NjA5OSwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMzAifX19.-s-lYvc-5nF461YsU9lH_ASeRfTttiwh-y3yn9L3r28'
+      Authorization: 'Bearer ' + token
     },
     data: params
   });
