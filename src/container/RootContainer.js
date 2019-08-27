d94b31cd63cefb2ce8a58efeb50319f6601b46b0
Khoa Mai <maidinhkhoa92@gmail.com>
2019-08-27 14:47:06 +0700

    fixed back button.

--- src/container/RootContainer.js
+++ src/container/RootContainer.js
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { IonApp, setupConfig } from '@ionic/react';
+import { Plugins } from '@capacitor/core';
+import history from 'utils/history';
 // Ionic 4 styles
 import '@ionic/core/css/core.css';
 import '@ionic/core/css/ionic.bundle.css';
@@ -12,12 +14,29 @@ setupConfig({
   hardwareBackButton: true
 });
 
-const RootContainer = props => {
-  return (
-    <IonApp>
-      <Suspense fallback={<Loading />}>{props.children}</Suspense>
-    </IonApp>
-  );
+class RootContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    };
+    this.app = Plugins.App; 
+    this.app.addListener('backButton', (url) => {
+      history.goBack()
+    });
+  }
+  render() {
+    const { children } = this.props;
+    if(this.state.loading) {
+      return <Loading />
+    }
+    return (
+      <IonApp>
+        <Suspense fallback={<Loading />}>{children}</Suspense>
+      </IonApp>
+    );
+  }
+  
 };
 RootContainer.propTypes = {
   children: PropTypes.node
